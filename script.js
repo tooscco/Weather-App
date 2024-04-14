b8f6bed67d7cddbb1aae99765e1dccdcf168e795
tooscco <aladejuyigbetosin7@gmail.com>
2024-04-14 15:29:26 +0100

    still working

--- script.js
+++ script.js
@@ -19,6 +19,8 @@
     let iconImage = collect.weather[0].icon
     let description = collect.weather[0].description
     let main = collect.weather[0].main
+    let temp_max = collect.main.temp_max
+    let temp_min =collect.main.temp_min
     showMeMap.innerHTML= `
     <h2 class="div-div-h4">${stateName}</h2>
     <h6>${main}</h6>
@@ -26,6 +28,16 @@
     <img class="div-img" src="https://openweathermap.org/img/w/${iconImage}.png">
     <h2 class="div-div-h2">${mainAns}°C</h2>
     <h3 class="div-div-h3">${country}</h3>
+    <div class="d-flex divScript">
+    <div class="me-1">
+    <h6>temp_max</h6>
+    <p class="p-div">${temp_max}</p>
+    </div><h6>|</h6>
+    <div class="ms-1">
+    <h6>temp_min</h6>
+    <p class="p-div">${temp_min}</p>
+    </div>
+    </div>
     `
     console.log(collect);
     }
